fc4ff8399750594d33c05017869e180da93a0599
tekihei2317 <tekihei2317@gmail.com>
2022-08-14 15:16:14 +0900

    diff3マージをやってみる

---
+++ diff3-merge/README.md
@@ -0,0 +1,11 @@
+# diff3-merge
+
+## モチベーション
+
+gitで使われているdiff3マージについて、実際にコードを動かして理解したい。
+
+## 方法
+
+gitのJavaScript実装である[isomorphic-git](https://github.com/isomorphic-git/isomorphic-git)のコードを参考にする。
+
+isomorphic-gitでファイルのマージは[mergeFile.js](https://github.com/isomorphic-git/isomorphic-git/blob/main/src/utils/mergeFile.js)に書かれており、diff3というライブラリを使用している。このmergeFile.jsをコピーして動かしてみて、diff3マージについて理解を深める。
--- isomorphic-git/examples/three-way-merge.js
+++ isomorphic-git/examples/three-way-merge.js
@@ -18,36 +18,47 @@ const git = require("isomorphic-git");
 const dir = "tutorial";
 const author = { name: "tekihei2317", email: "tekihei2317@gmail.com" };
 
-async function main() {
-  // リポジトリを初期化する
+async function initRepository() {
   await fs.promises.rm(path.join(dir, ".git"), {
     recursive: true,
     force: true,
   });
   await git.init({ fs, dir, defaultBranch: "main" });
   console.log("Initialized git repository.");
+}
 
-  // first commitを作成する
+async function commit1() {
   await fs.promises.writeFile(path.join(dir, "README.md"), "# three-way-merge\n");
   await git.add({ fs, dir, filepath: "README.md" });
-  await git.commit({ fs, dir, message: "added README.md", author });
-  console.log("Created first commit.");
+  // await git.commit({ fs, dir, message: "added README.md", author });
+  // console.log("Created first commit.");
+}
 
+async function commit2() {
   // practiceブランチでコミットする
   await git.branch({ fs, dir, ref: "practice" });
   await git.checkout({ fs, dir, ref: "practice" });
-  // await fs.promises.writeFile(path.join(dir, "test.txt"), "Hello");
+
   await fs.promises.writeFile(path.join(dir, "README.md"), "# three-way-merge\n\nマージのアルゴリズムが知りたい\n");
   await git.add({ fs, dir, filepath: "." });
   await git.commit({ fs, dir, message: "modify README.md", author });
   console.log("Created commit on practice.");
+}
 
-  // mainブランチにpracticeブランチをマージする
+async function mergePracticeIntoMain() {
   // FIXME: マージがバグっている気がする(インデックスがコミットに反映されていない)
   await git.checkout({ fs, dir, ref: "main" });
   const mergeResult = await git.merge({ fs, dir, theirs: "practice", author, fastForward: true });
   console.log("Merge practice branch into main.");
   console.log(mergeResult);
+}
+
+async function main() {
+  // リポジトリを初期化する
+  await initRepository();
+  await commit1();
+  // await commit2();
+  // await mergePracticeIntoMain();
 }
 
 main();
