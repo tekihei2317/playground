cdd425630c96a975ea3dbfdfc00c11f5234c855b
tekihei2317 <tekihei2317@gmail.com>
2022-10-16 15:44:58 +0900

    zodiosでAPIを作成

---
+++ zodios/backend/src/index.ts
@@ -0,0 +1,79 @@
+import { makeApi } from "@zodios/core";
+import { zodiosApp } from "@zodios/express";
+import { z } from "zod";
+
+const CreateUserSchema = z.object({
+  name: z.string().max(10),
+  email: z.string(),
+});
+const UserSchema = z.object({
+  id: z.number(),
+  name: z.string().max(10),
+  email: z.string(),
+});
+const UsersSchema = z.array(UserSchema);
+
+type User = z.infer<typeof UserSchema>;
+
+const api = makeApi([
+  {
+    method: "get",
+    path: "/users/:id",
+    response: UserSchema,
+    alias: "getUser",
+    description: "Get user",
+  },
+  {
+    method: "get",
+    path: "/users",
+    response: UsersSchema,
+    alias: "getUsers",
+    description: "Get users",
+  },
+  {
+    method: "post",
+    path: "/users",
+    response: UserSchema,
+    alias: "createUser",
+    description: "Create user",
+    parameters: [
+      {
+        name: "body",
+        type: "Body",
+        schema: CreateUserSchema,
+      },
+    ],
+  },
+]);
+
+const app = zodiosApp(api);
+
+let userId = 1;
+const users: User[] = [];
+
+app.get("/users/:id", (req, res) => {
+  // TODO: パスパラメーターに型をつけたい
+  const userId = Number(req.params.id);
+  const user = users[userId];
+
+  if (user === undefined) {
+    return res.status(404).send();
+  }
+
+  return res.json(user);
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", (req, res) => {
+  const newUser = { ...req.body, id: userId++ };
+  users.push(newUser);
+
+  res.json(newUser);
+});
+
+app.listen(3000, function () {
+  console.log("server is listening at port 3000");
+});
