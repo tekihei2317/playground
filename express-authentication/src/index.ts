f00e528303c979a07cce9b288da332b03794df1d
tekihei2317 <tekihei2317@gmail.com>
2022-11-01 10:16:02 +0900

    expressをインストール

---
+++ express-authentication/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.send({ hello: "world" });
+});
+
+app.listen(port, function () {
+  console.log(`server is listening at port ${port}.`);
+});
