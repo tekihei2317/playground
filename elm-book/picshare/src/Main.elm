0d6d2ab789e18e65b515cce3d85f8649076c2665
tekihei2317 <tekihei2317@gmail.com>
2022-11-03 20:50:26 +0900

    JSONデコーダーを作成

--- elm-book/picshare/src/Main.elm
+++ elm-book/picshare/src/Main.elm
@@ -1,9 +1,11 @@
-module Main exposing (main)
+module Main exposing (main, photoDecoder)
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class, disabled, placeholder, src, type_, value)
 import Html.Events exposing (onClick, onInput, onSubmit)
+import Json.Decode exposing (Decoder, bool, int, list, string, succeed)
+import Json.Decode.Pipeline exposing (hardcoded, required)
 
 
 baseUrl : String
@@ -11,13 +13,33 @@ baseUrl =
     "https://programming-elm.com"
 
 
+type alias Id =
+    Int
+
+
+type alias Photo =
+    { id : Id, url : String, caption : String, liked : Bool, comments : List String, newComment : String }
+
+
 type alias Model =
-    { url : String, caption : String, liked : Bool, comments : List String, newComment : String }
+    Photo
+
+
+photoDecoder : Decoder Photo
+photoDecoder =
+    succeed Photo
+        |> required "id" int
+        |> required "url" string
+        |> required "caption" string
+        |> required "liked" bool
+        |> required "comments" (list string)
+        |> hardcoded ""
 
 
 initialModel : Model
 initialModel =
-    { url = baseUrl ++ "/1.jpg"
+    { id = 1
+    , url = baseUrl ++ "/1.jpg"
     , caption = "Surfing"
     , liked = False
     , comments = [ "いい波乗ってんね！" ]
