b50ee4db552164e1584ba2da19ded2bbc42c00cc
tekihei2317 <tekihei2317@gmail.com>
2022-11-10 15:06:41 +0900

    React Suspense

--- react-suspense/src/App.tsx
+++ react-suspense/src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Suspense, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function sleep(ms: number) {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      // console.log("Promise resolved");
+      resolve("success");
+    }, ms)
+  );
 }
 
-export default App
+// プロミスをスローすることが、描画を停止することを表す
+// スローしたプロミスが解決したときに、コンポーネントは再描画される
+export const SometimesSuspend = () => {
+  if (Math.random() < 0.5) {
+    throw sleep(1000);
+  }
+  return <p>Hello, world!</p>;
+};
+
+async function fetchData1(): Promise<string> {
+  await sleep(1000);
+  return `Hello, ${(Math.random() * 1000).toFixed(0)}`;
+}
+
+const DataLoader = () => {
+  const [data, setData] = useState<string>();
+  const [loading, setLoading] = useState(false);
+
+  // 初回でサスペンドするとステートの記憶領域が確保されないため、初回はサスペンドしない
+  if (loading && data === undefined) {
+    throw fetchData1().then(setData);
+  }
+
+  return (
+    <div>
+      <div>Data is {data}</div>
+      {/* クリックするとサスペンドする */}
+      <button onClick={() => setLoading(true)}>load</button>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <div>React App!</div>
+      <Suspense fallback={<p>loading...</p>}>
+        <DataLoader />
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
