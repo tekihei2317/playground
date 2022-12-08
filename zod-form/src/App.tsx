f1f6320f0b7d5061850a6f659ec64346ec5fcfbe
tekihei2317 <tekihei2317@gmail.com>
2022-12-08 14:48:28 +0900

    zodとreact-hook-formを組み合わせたフォーム

--- zod-form/src/App.tsx
+++ zod-form/src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const stringSchema = z.string().min(1);
+const dateString = z
+  .string()
+  .regex(/^[0-9]{8}$/, { message: "日付は正しい形式で入力してください" });
+
+const registrationFormSchema = z.object({
+  userName: stringSchema.max(15),
+  email: stringSchema.email(),
+  birthDate: dateString,
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<z.infer<typeof registrationFormSchema>>({
+    resolver: zodResolver(registrationFormSchema),
+  });
+  const onSubmit = handleSubmit((d) => console.log(d));
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="py-4">
+      <form
+        onSubmit={onSubmit}
+        className="flex flex-col gap-4 items-start max-w-xl mx-auto"
+      >
+        <label>
+          ユーザー名
+          <input {...register("userName")} className="border" />
+          <p className="text-sm text-red-500">{errors.userName?.message}</p>
+        </label>
+        <label>
+          メールアドレス
+          <input {...register("email")} className="border" />
+          <p className="text-sm text-red-500">{errors.email?.message}</p>
+        </label>
+        <label>
+          生年月日
+          <input {...register("birthDate")} className="border" />
+          <p className="text-sm text-red-500">{errors.birthDate?.message}</p>
+        </label>
+        <button type="submit" className="bg-blue-200 px-4 py-2 rounded">
+          登録
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
